48af59bc7306c23ffd1a6549e3989958716d5624
yanjuan <payq123456>
2021-03-05 19:13:23 +0800

    新增 跃动族 app

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -96,3 +96,9 @@
 10 7 * * * node /scripts/jd_ms.js >> /scripts/logs/jd_ms.log 2>&1
 # 删除优惠券(默认注释，如需要自己开启，如有误删，已删除的券可以在回收站中还原，慎用)
 #20 9 * * 6 node /scripts/jd_delCoupon.js >> /scripts/logs/jd_delCoupon.log 2>&1
+
+
+## app
+
+## 跃动族
+0,30 0-23 * * * node /scripts/yuedongzu.js >> /scripts/yuedongzu.log 2>&1
